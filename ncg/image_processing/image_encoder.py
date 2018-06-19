a59b9dfeb3581d2b149b54191d85b5b6406f2b2d
maartje <maartjedejonge@gmail.com>
2018-06-19 10:05:20 +0200

    refactor image encoder and add ttests

---
+++ ncg/image_processing/image_encoder.py
@@ -0,0 +1,48 @@
+from torchvision import models, transforms
+import torch
+
+class ImageEncoder:
+
+    def __init__(self, model_name, layer_name, encoder_models = {}):
+        self.model_name = model_name
+        self.layer_name = layer_name
+        self.model = None
+        self.layer = None
+        self.encoder_models = {
+            'resnet18': models.resnet18,
+            'resnet152': models.resnet152,
+            'vgg16': models.vgg16
+        }
+        self.encoder_models.update(encoder_models)
+        self.transform = transforms.Compose([
+            transforms.Resize(224),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean = [0.485, 0.456, 0.406],
+                                 std = [0.229, 0.224, 0.225]),
+        ])
+
+
+    def load_model(self):
+        self.model = self.encoder_models[self.model_name](pretrained=True) 
+        self.layer = self.model._modules.get(self.layer_name)
+        self.model.eval()
+
+    def encode_image(self, img):
+        t_img = self.transform(img).unsqueeze(0)
+        return self._embed(t_img)
+
+    def encode_images(self, imgages):
+        t_imgages = torch.stack([self.transform(img) for img in imgages])
+        return self._embed(t_imgages)
+
+    def _embed(self, t_img):
+        embedding = [] # stores the output for the given layer
+        def copy_data(m, i, o):
+            embedding.append(o.clone())
+        h = self.layer.register_forward_hook(copy_data)
+        self.model(t_img)
+        h.remove()    
+        return embedding[0].squeeze()
+
+
---
+++ test/test_image_encoder.py
@@ -0,0 +1,56 @@
+"""
+Tests for the image encoder that maps images to ebedding vectors
+"""
+
+import unittest
+from ncg.image_processing.image_encoder import ImageEncoder
+import numpy
+from PIL import Image
+
+class TestImageEncoder(unittest.TestCase):
+
+    def setUp(self):
+        self.image_encoder = ImageEncoder('resnet18', 'avgpool')
+        self.image_encoder.load_model()
+        
+    def test_encode_image_large(self):
+        # create random imagewidth, height
+        img = self.create_random_image(620, 580)
+        img_encoding = self.image_encoder.encode_image(img)
+        
+        self.assertEqual(list(img_encoding.size()), [512])
+        self.assertTrue(sum(img_encoding.data) > 0)
+
+    def test_encode_image_small(self):
+        # create random imagewidth, height
+        img = self.create_random_image(120, 134)
+        img_encoding = self.image_encoder.encode_image(img)
+        
+        self.assertEqual(list(img_encoding.size()), [512])
+        self.assertTrue(sum(img_encoding.data) > 0)
+
+    def test_encode_images(self):
+        img1 = self.create_random_image(620, 580)
+        img2 = self.create_random_image(120, 134)
+        images = [img1, img2]
+        emb1 = self.image_encoder.encode_image(img1)
+        emb2 = self.image_encoder.encode_image(img2)
+        
+        emb_all = self.image_encoder.encode_images(images)
+        
+        emb_all_1 = emb_all[0,:]
+        emb_all_2 = emb_all[1,:]
+        self.assertEqual(list(emb_all.size()), [2, 512])
+        self.assertEqual(emb1[120], emb_all_1[120])
+        self.assertEqual(emb2[110], emb_all_2[110])
+                
+    def create_random_image(self, width, height):
+        imarray = numpy.random.rand(width, height, 3) * 255
+        img = Image.fromarray(imarray.astype('uint8')).convert('RGB')
+        return img
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
