35cf6f62a5f20e54fea339f0e0bc96f592f0aea8
maartje <maartjedejonge@gmail.com>
2018-06-22 15:05:03 +0200

    implement function to train show and tell model

---
+++ ncg/models/show_and_tell.py
@@ -0,0 +1,21 @@
+import torch
+
+from ncg.models.models import DecoderRNN
+
+class ShowAndTell():
+
+    def __init__(self, hidden_size, output_size, device):
+        self.device = device
+        self.decoder = DecoderRNN(hidden_size, output_size)
+        self.decoder.to(self.device)
+            
+    def calculate_output_probabilities(self, source_encoding, inputs):
+        hidden = source_encoding.view(1,1,-1)
+        output_probs = torch.zeros(inputs.size(0), self.decoder.output_size, device=self.device)
+        for i, input_token in enumerate(inputs):
+            output, hidden = self.decoder(input_token, hidden)
+            output_probs[i] = output
+        return output_probs
+
+
+
