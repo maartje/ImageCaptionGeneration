447d2cab084eb4095346f849eed3f85dc2808a30
maartje <maartjedejonge@gmail.com>
2018-06-29 10:20:31 +0200

    refactor loss collector

--- ncg/reporting/loss_collector.py
+++ ncg/reporting/loss_collector.py
@@ -5,29 +5,45 @@ class LossCollector():
         self.epoch_losses = []
         self.batch_losses = []
         self._tmp_losses = []
-        # TODO: epoch size, batch_sizes (for diagrams)
+        self.epoch_size = None # determined during processing
+        self.batch_loss_size_last = 0 # determined during processing
         
-    def process_loss(self, epoch, batch_index, token_loss, epoch_finished):
+    def process_train_loss(self, epoch, batch_index, token_loss, epoch_finished):
+        self.process_tmp_loss(epoch, batch_index, token_loss, epoch_finished)
+        self.process_batch_loss(epoch, batch_index, token_loss, epoch_finished)
+        self.process_epoch_loss(epoch, batch_index, token_loss, epoch_finished)
+
+    # Step 1: add loss to _tmp_losses, clear _tmp_losses when new epoch has started
+    def process_tmp_loss(self, epoch, batch_index, token_loss, epoch_finished):
+        if batch_index == 0:        
+            self._tmp_losses = []
         self._tmp_losses.append(token_loss)
+
+    # Step 2: store batch loss in batch losses, when batch interval
+    def process_batch_loss(self, epoch, batch_index, token_loss, epoch_finished):
         batch_mod = len(self._tmp_losses) % self.batch_loss_size
         if batch_mod == 0 and len(self._tmp_losses):
             if epoch >= len(self.batch_losses):
                 self.batch_losses.append([])
-            losses_current_batch = self._tmp_losses[-self.batch_loss_size:]
-#            print('store batch', epoch, batch_index, losses_current_batch)
-            average_batch_loss = sum(losses_current_batch)/len(losses_current_batch)
-            self.batch_losses[epoch].append(average_batch_loss)
+            self.store_batch_loss(self.batch_loss_size)
+        elif epoch_finished:
+            self.batch_loss_size_last = batch_mod
+            self.store_batch_loss(batch_mod)
+            
+    def store_batch_loss(self, batch_size):
+        batch_losses = self._tmp_losses[-batch_size : ]
+        average_batch_loss = sum(batch_losses)/len(batch_losses)
+        self.batch_losses[-1].append(average_batch_loss)
+#        print('store partial batch', epoch, batch_index, losses_partial_batch)
+        
+
+
+    # Step 3: store epoch loss in epoch_losses when epoch has finished
+    def process_epoch_loss(self, epoch, batch_index, token_loss, epoch_finished):
         if epoch_finished:        
             average_epoch_loss = sum(self._tmp_losses)/ len(self._tmp_losses)
             self.epoch_losses.append(average_epoch_loss)
+            self.epoch_size = batch_index + 1
 #            print('store epoch', epoch, batch_index, losses[0])
             
-            if batch_mod:
-                losses_partial_batch = self._tmp_losses[-batch_mod:]
-                average_partial_batch_loss = sum(losses_partial_batch)/len(losses_partial_batch)
-                self.batch_losses[-1].append(average_partial_batch_loss)
-#                print('store partial batch', epoch, batch_index, losses_partial_batch)
-            
-            self._tmp_losses = []
-
 
--- test/test_loss_collector.py
+++ test/test_loss_collector.py
@@ -17,22 +17,26 @@ class TestLossCollector(unittest.TestCase):
         for e, losses_in_epoch in enumerate(self.epoch_losses):
             for i, loss in enumerate(losses_in_epoch):
                 epoch_finished = (i + 1 == len(losses_in_epoch))
-                loss_collector.process_loss(e, i, loss, epoch_finished)
+                loss_collector.process_train_loss(e, i, loss, epoch_finished)
         epoch_losses_expected = [5, 50]
         batch_losses_expected = [[2.5, 6.5, 9], [25, 65, 90]]
         self.assertEqual(epoch_losses_expected, loss_collector.epoch_losses)
         self.assertEqual(batch_losses_expected, loss_collector.batch_losses)
+        self.assertEqual(len(self.epoch_losses[0]), loss_collector.epoch_size)
+        self.assertEqual(1, loss_collector.batch_loss_size_last)
 
     def test_process_loss_without_partial(self):
         loss_collector = LossCollector(3)
         for e, losses_in_epoch in enumerate(self.epoch_losses):
             for i, loss in enumerate(losses_in_epoch):
                 epoch_finished = (i + 1 == len(losses_in_epoch))
-                loss_collector.process_loss(e, i, loss, epoch_finished)
+                loss_collector.process_train_loss(e, i, loss, epoch_finished)
         epoch_losses_expected = [5, 50]
         batch_losses_expected = [[2,5,8], [20,50,80]]
         self.assertEqual(epoch_losses_expected, loss_collector.epoch_losses)
         self.assertEqual(batch_losses_expected, loss_collector.batch_losses)
+        self.assertEqual(len(self.epoch_losses[0]), loss_collector.epoch_size)
+        self.assertEqual(0, loss_collector.batch_loss_size_last)
 
 
 if __name__ == '__main__':
