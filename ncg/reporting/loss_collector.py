9256ba503dff8454924d45f603fbb379e7073117
maartje <maartjedejonge@gmail.com>
2018-06-29 10:43:34 +0200

    rename

--- ncg/reporting/loss_collector.py
+++ ncg/reporting/loss_collector.py
@@ -9,18 +9,18 @@ class LossCollector():
         self.batch_loss_size_last = 0 # determined during processing
         
     def process_train_loss(self, epoch, batch_index, token_loss, epoch_finished):
-        self.process_tmp_loss(epoch, batch_index, token_loss, epoch_finished)
-        self.process_batch_loss(epoch, batch_index, token_loss, epoch_finished)
-        self.process_epoch_loss(epoch, batch_index, token_loss, epoch_finished)
+        self.update_tmp_loss(epoch, batch_index, token_loss, epoch_finished)
+        self.update_batch_loss(epoch, batch_index, token_loss, epoch_finished)
+        self.update_epoch_loss(epoch, batch_index, token_loss, epoch_finished)
 
     # Step 1: add loss to _tmp_losses, clear _tmp_losses when new epoch has started
-    def process_tmp_loss(self, epoch, batch_index, token_loss, epoch_finished):
+    def update_tmp_loss(self, epoch, batch_index, token_loss, epoch_finished):
         if batch_index == 0:        
             self._tmp_losses = []
         self._tmp_losses.append(token_loss)
 
     # Step 2: store batch loss in batch losses, when batch interval
-    def process_batch_loss(self, epoch, batch_index, token_loss, epoch_finished):
+    def update_batch_loss(self, epoch, batch_index, token_loss, epoch_finished):
         batch_mod = len(self._tmp_losses) % self.batch_loss_size
         if batch_mod == 0 and len(self._tmp_losses):
             if epoch >= len(self.batch_losses):
@@ -36,10 +36,8 @@ class LossCollector():
         self.batch_losses[-1].append(average_batch_loss)
 #        print('store partial batch', epoch, batch_index, losses_partial_batch)
         
-
-
     # Step 3: store epoch loss in epoch_losses when epoch has finished
-    def process_epoch_loss(self, epoch, batch_index, token_loss, epoch_finished):
+    def update_epoch_loss(self, epoch, batch_index, token_loss, epoch_finished):
         if epoch_finished:        
             average_epoch_loss = sum(self._tmp_losses)/ len(self._tmp_losses)
             self.epoch_losses.append(average_epoch_loss)
