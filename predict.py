303a00899328e758a4f4f364a48ea77aacdc1303
maartje <maartjedejonge@gmail.com>
2018-07-03 14:25:15 +0200

    working on prediction

---
+++ predict.py
@@ -0,0 +1,27 @@
+import torch
+from ncg.nn.train_model import predict
+from torch.utils import data
+from ncg.data_processing.image_encoder import ImageEncoder
+
+def predict(fpaths_images, fpath_decoder, fpath_vocab,
+            fpath_save_predictions, encoder_model, encoder_layer,
+            max_length = 100, dl_params = {}):
+
+    # create models and data
+    image_encoder = ImageEncoder(encoder_model, encoder_layer)
+    image_encoder.load_model()
+    decoder = torch.load(fpath_decoder)
+    text_mapper = torch.load(fpath_vocab)
+    data_set = ImageDataset(fpaths_images)
+    data_loader = data.DataLoader(data_set)
+
+    # predict captions
+    predicted_sentences = []
+    for img in data_loader:
+        image_encoding = image_encoder.encode_image(img)
+        predicted_indices = predict(
+            decoder, image_encoding, text_mapper.SOS, text_mapper.EOS, max_length)
+        predicted_sentence = text_mapper.indices2sentence(prediction)
+        predicted_sentences.append(predicted_sentence)
+
+    open(fpath_save_predictions, "w").write('\n'.join(predicted_sentences))
