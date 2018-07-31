38fb65ed507a35c675249f50dbd97b3abe9039be
Maartje de Jonge <maartjej@test3.irc.sara.nl.irc.sara.nl>
2018-07-31 16:02:16 +0200

    experiment with jobs

--- bar.py
+++ bar.py
@@ -1,3 +1,5 @@
+import sys
+
 def main():
     print('hello from python!')
 
--- bar.sh
+++ bar.sh
@@ -2,10 +2,17 @@
 #PBS -S /bin/bash
 #PBS -lnodes=1
 #PBS -lwalltime=0:00:30
+# -qexpress_gpu
+# -qgpu
 
 #Loading modules
 # TODO
+# source activate summer_project
 source activate summer_project
+#module load python/3.7.0
+module load eb
+module load CUDA
+#module load cudnn
 
 #Copy input data to scratch and create output directory
 #mkdir "$TMPDIR"/flickr30k
@@ -14,9 +21,13 @@ source activate summer_project
 
 #Run program
 #python3 -W ignore train.py --config config_lisa.json
+#which nvcc
+#echo 'mj'
+#nvcc --version
 echo 'hello world!' 
-python3 bar.py
-echo 'hello from bash!' 
+
+python3 ImageCaptionGeneration/bar.py
+#echo 'hello from bash!' 
 
 #Copy output data from scratch to home
 #cp -r "$TMPDIR"/output $HOME
