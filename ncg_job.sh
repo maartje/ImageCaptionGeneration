e14900b4c8694cdbc7e0112633437f16e748c5ff
maartje <maartjedejonge@gmail.com>
2018-07-31 11:06:07 +0200

    working on lisa cluster

---
+++ ncg_job.sh
@@ -0,0 +1,18 @@
+#Set job requirements
+#PBS -S /bin/bash
+#PBS -lnodes=1
+#PBS -lwalltime=1:30:00
+
+#Loading modules
+module load python/3.5.0
+
+#Copy input data to scratch and create output directory
+mkdir "$TMPDIR"/flickr30k
+mkdir "$TMPDIR"/output
+cp -r $HOME/flickr30k "$TMPDIR"/flickr30k
+
+#Run program
+python3 train.py --config config_lisa.json
+
+#Copy output data from scratch to home
+cp -r "$TMPDIR"/output $HOME
