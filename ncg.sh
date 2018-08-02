bdb6eaa0f4835a20241cdbf5a6579a6cb58e77f5
Maartje de Jonge <maartjej@test3.irc.sara.nl.irc.sara.nl>
2018-08-02 12:23:12 +0200

    working on lisa run

--- ncg.sh
+++ ncg.sh
@@ -13,9 +13,13 @@ module load CUDA
 module load cuDNN
 
 #Copy input data to scratch and create output directory
-mkdir "$TMPDIR"/flickr30k
 mkdir "$TMPDIR"/output
-cp -r $HOME/flickr30k "$TMPDIR"/flickr30k
+cp -r "$HOME"/ImageCaptionGeneration/flickr30k "$TMPDIR"
+echo "$TMPDIR"/flickr30k
+echo $TMPDIR/flickr30k
+echo "   do ls on TMPDIR/flickr30k"
+ls "$TMPDIR"/flickr30k
+ls $TMPDIR/flickr30k
 
 #Run program
 cd ImageCaptionGeneration
@@ -23,4 +27,4 @@ cd ImageCaptionGeneration
 python3 -W ignore train.py --config config_lisa.json
 
 #Copy output data from scratch to home
-cp -r "$TMPDIR"/output $HOME
+cp -r "$TMPDIR"/output "$HOME"/ImageCaptionGeneration
