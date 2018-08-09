131e94c407cd678801b0df948472db628ab11a38
maartje <maartjedejonge@gmail.com>
2018-08-09 20:48:36 +0200

    add scripts for full run 2

---
+++ job_scripts/predict_2.sh
@@ -0,0 +1,31 @@
+#Set job requirements
+#PBS -S /bin/bash
+#PBS -lnodes=1
+#PBS -lwalltime=2:00:00
+#PBS -qgpu
+
+#Loading modules
+source activate summer_project
+module load eb
+module load CUDA
+module load cuDNN
+
+#Copy input data to scratch 
+cp -r "$HOME"/ImageCaptionGeneration/flickr30k/preprocess "$TMPDIR"
+cp -r "$HOME"/ImageCaptionGeneration/flickr30k/train "$TMPDIR"
+
+#Run program
+CUDA_VISIBLE_DEVICES=0 python3 -W ignore "$HOME"/ImageCaptionGeneration/predict.py --config "$HOME"/ImageCaptionGeneration/configs/config.json --model_dir show_tell_sgd_05 &
+
+CUDA_VISIBLE_DEVICES=1 python3 -W ignore "$HOME"/ImageCaptionGeneration/predict.py --config "$HOME"/ImageCaptionGeneration/configs/config.json --model_dir show_tell_adam_min5 &
+
+CUDA_VISIBLE_DEVICES=2 python3 -W ignore "$HOME"/ImageCaptionGeneration/predict.py --config "$HOME"/ImageCaptionGeneration/configs/config.json --model_dir show_tell_adam_min4 &
+
+CUDA_VISIBLE_DEVICES=3 python3 -W ignore "$HOME"/ImageCaptionGeneration/predict.py --config "$HOME"/ImageCaptionGeneration/configs/config.json --model_dir show_tell_adam_min3 &
+
+wait
+
+#Copy output data to persistent disk 
+cp -r "$TMPDIR"/predict "$HOME"/ImageCaptionGeneration/flickr30k
+
+
