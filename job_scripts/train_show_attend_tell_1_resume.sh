b2b5dadd86da0764823fd559a797df1a0603f6bd
maartje <maartjedejonge@gmail.com>
2018-08-28 17:11:49 +0200

    do not resume adam

--- job_scripts/train_show_attend_tell_1_resume.sh
+++ job_scripts/train_show_attend_tell_1_resume.sh
@@ -1,7 +1,7 @@
 #Set job requirements
 #PBS -S /bin/bash
 #PBS -lnodes=1
-#PBS -lwalltime=010:00:00
+#PBS -lwalltime=09:00:00
 #PBS -qgpu
 
 #Loading modules
@@ -19,15 +19,10 @@ cp "$HOME"/ImageCaptionGeneration/flickr30k/train/show_attend_tell_sgd_1/* "$TMP
 mkdir -p "$TMPDIR"/train/show_attend_tell_sgd_1_alpha_01
 cp "$HOME"/ImageCaptionGeneration/flickr30k/train/show_attend_tell_sgd_1_alpha_01/* "$TMPDIR"/train/show_attend_tell_sgd_1_alpha_01
 
-mkdir -p "$TMPDIR"/train/show_attend_tell_adam_1min3
-cp "$HOME"/ImageCaptionGeneration/flickr30k/train/show_attend_tell_adam_1min3/* "$TMPDIR"/train/show_attend_tell_adam_1min3
-
 #Run program
-CUDA_VISIBLE_DEVICES=0 python3 -W ignore "$HOME"/ImageCaptionGeneration/train.py --config "$HOME"/ImageCaptionGeneration/configs/config_flickr30k_resume.json --model_dir show_attend_tell_sgd_1 --learning_rate 1.0 --optimizer SGD  --model show_attend_tell --alpha_c 1.0 --fname_resume model.21.pt &
+CUDA_VISIBLE_DEVICES=0 python3 -W ignore "$HOME"/ImageCaptionGeneration/train.py --config "$HOME"/ImageCaptionGeneration/configs/config_flickr30k_resume.json --model_dir show_attend_tell_sgd_1 --learning_rate 0.25 --optimizer SGD  --model show_attend_tell --alpha_c 1.0 --fname_resume model.21.pt &
 
-CUDA_VISIBLE_DEVICES=1 python3 -W ignore "$HOME"/ImageCaptionGeneration/train.py --config "$HOME"/ImageCaptionGeneration/configs/config_flickr30k_resume.json --model_dir show_attend_tell_sgd_1_alpha_01 --learning_rate 1.0 --optimizer SGD  --model show_attend_tell --alpha_c 0.1 --fname_resume model.21.pt &
-
-CUDA_VISIBLE_DEVICES=3 python3 -W ignore "$HOME"/ImageCaptionGeneration/train.py --config "$HOME"/ImageCaptionGeneration/configs/config_flickr30k_resume.json --model_dir show_attend_tell_adam_1min3 --learning_rate 0.001 --optimizer ADAM  --model show_attend_tell  --alpha_c 1.0 --fname_resume model.21.pt &
+CUDA_VISIBLE_DEVICES=1 python3 -W ignore "$HOME"/ImageCaptionGeneration/train.py --config "$HOME"/ImageCaptionGeneration/configs/config_flickr30k_resume.json --model_dir show_attend_tell_sgd_1_alpha_01 --learning_rate 0.25 --optimizer SGD  --model show_attend_tell --alpha_c 0.1 --fname_resume model.21.pt &
 
 wait
 
@@ -38,6 +33,4 @@ cp "$TMPDIR"/train/show_attend_tell_sgd_1/* "$HOME"/ImageCaptionGeneration/flick
 mkdir -p "$HOME"/ImageCaptionGeneration/flickr30k/train/show_attend_tell_sgd_1_alpha_01_resume
 cp "$TMPDIR"/train/show_attend_tell_sgd_1_alpha_01/* "$HOME"/ImageCaptionGeneration/flickr30k/train/show_attend_tell_sgd_1_alpha_01_resume
 
-mkdir -p "$HOME"/ImageCaptionGeneration/flickr30k/train/show_attend_tell_adam_1min3_resume
-cp "$TMPDIR"/train/show_attend_tell_adam_1min3/* "$HOME"/ImageCaptionGeneration/flickr30k/train/show_attend_tell_adam_1min3_resume
 
